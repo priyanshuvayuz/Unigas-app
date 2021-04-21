726af49e91090e613ee38f934f4cd34e1810518f
Priyanshu Baluni <80383528+priyanshuvayuz@users.noreply.github.com>
2021-04-21 11:39:42 +0530

    files

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import {typography} from './src/components/typography'
+typography()
+AppRegistry.registerComponent(appName, () => App);
